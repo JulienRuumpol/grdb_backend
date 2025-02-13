942827c844796bdbd2fd5fe9bad3f9d99056b16b
Julien <jruumpol@hotmail.com>
2025-02-13 12:53:34 +0100

    [add] swagger and remove authentication requirement

---
+++ src/main/java/com/jr/grdb_backend/config/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.jr.grdb_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfiguration {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //todo set authentication and authorization
+        // currently this will allow all requests to go through without authentication
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+
+    }
+}
+
