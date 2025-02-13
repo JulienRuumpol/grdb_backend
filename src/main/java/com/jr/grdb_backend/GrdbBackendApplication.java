8ac1680ff5b180a637074221e678eeda03569cfa
Julien <jruumpol@hotmail.com>
2025-02-13 10:40:11 +0100

    initial commit

---
+++ src/main/java/com/jr/grdb_backend/GrdbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jr.grdb_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrdbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrdbBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jr/grdb_backend/GrdbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jr.grdb_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrdbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
