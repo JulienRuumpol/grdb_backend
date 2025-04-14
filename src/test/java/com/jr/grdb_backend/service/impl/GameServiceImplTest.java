2a9411d676e8e830176c82a04b7b76d640a8f58d
Julien <jruumpol@hotmail.com>
2025-04-14 16:31:42 +0200

    [add] setup GameServiceImplTest

--- src/test/java/com/jr/grdb_backend/service/impl/GameServiceImplTest.java
+++ src/test/java/com/jr/grdb_backend/service/impl/GameServiceImplTest.java
@@ -1,14 +1,41 @@
 package com.jr.grdb_backend.service.impl;
 
+import com.jr.grdb_backend.model.Game;
+import com.jr.grdb_backend.repository.GameRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class GameServiceImplTest {
 
+    @InjectMocks
+    private GameServiceImpl gameServiceImpl;
+    @Mock
+    private GameRepository gameRepository;
+
+    private Game game;
+
+
+    @BeforeEach
+    void setUp() {
+        this.game = Game.builder()
+                .description("A game used for testing purpose")
+                .id(1l)
+                .name("Testing: The video game")
+                .build();
+    }
+
+    /**
+     * Unit test {@link GameServiceImpl#getAll()}
+     * Scenario: Successfully retrieve all games
+     */
     @Test
     void getAll() {
+
     }
 
     @Test
